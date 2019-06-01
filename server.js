5f77b9f94cbf3080900fbfe9076b0cd368d3ee0b
ChaddFrasier <cmf339@nau.edu>
2019-05-31 19:27:14 -0700

    all three pages navigate properly

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
+/**
+ * Caption Writer Application
+ * 
+ * Author: Chadd Frasier
+ * Date: 5/31/19
+ * Version: 2.1
+ * Description: 
+ *      This is the driver for the Caption Writer server 
+ */
+
+// require dependencies
 const express = require('express');
 const path = require('path');
 const jimp = require('jimp');
 
-
+// start app env
 var app = express();
-// allow static files to be seen and served from any directory
 
+// give app access to routes
 app.use("/css" , express.static("css"));
-app.use("/images" , express.static("images"));
+app.use("/images" , express.static("images"));+
 
+// start view engine
 app.set('view engine', 'ejs');
 
+// index page
 app.get('/', function(request, response){
     console.log(request.path);
     response.render("index.ejs");
 });
 
+// caption writing page
 app.post('/upload', function(request, response){
     console.log(request.path);
     response.render("writer.ejs");
 });
 
+// image editing page
+app.post('/showImage', function(request, response){
+    console.log(request.path);
+    response.render("imagePage.ejs");
+});
 
 // listen on 3030
 const PORT = 8080 || process.env.PORT;
 app.listen(PORT);
-// tell console
+
+// tell console status
 console.log("Server is running and listen to port " + PORT);
