5775a64a19e9b61c6eeb4781f1026919f4d6b66e
ChaddFrasier <cmf339@nau.edu>
2019-05-31 18:51:26 -0700

    basic 2 page navigation

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var path = require('path');
-var jimp = require('jimp');
+const express = require('express');
+const path = require('path');
+const jimp = require('jimp');
+
 
 var app = express();
+// allow static files to be seen and served from any directory
+
+app.use("/css" , express.static("css"));
+app.use("/images" , express.static("images"));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response){
-    response.send('<h1>HELLO WORLD</h1>');
+    console.log(request.path);
+    response.render("index.ejs");
 });
+
+app.post('/upload', function(request, response){
+    console.log(request.path);
+    response.render("writer.ejs");
+});
+
 
 // listen on 3030
 const PORT = 8080 || process.env.PORT;
