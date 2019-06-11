e26e0c61165ea786b295d1dc3a3551ecd673b3e0
Chadd Frasier <33643771+ChaddFrasier@users.noreply.github.com>
2019-06-11 09:51:50 -0700

    Update util.js

--- util.js
+++ util.js
@@ -3,8 +3,8 @@
  * 
  * Author: Chadd Frasier
  * Date Created: 06/03/19
- * Date Last Modified: 06/04/19
- * Version: 1.1
+ * Date Last Modified: 06/10/19
+ * Version: 1.2.1
  * Description: 
  *      This is the utility file for The Planetary Image Caption Writer  
  */
@@ -14,81 +14,82 @@ const exec = require('child_process').exec;
 
 
 // local functions
+/**
+ * 
+ * @param {string} cubeName 
+ * @param {string} format 
+ * @returns {string} image name
+ * @description This function takes the file extension off of he cube file and makes it a png.
+ */
 var getimagename = function(cubeName, format){
+    // get an array of peieces of the filename
     let namearr = cubeName.split('.');
-
-    namearr[namearr - 1] = format;
+    // set the last element of the array to the format specified
+    namearr[namearr.length - 1] = format;
+    console.log(namearr);
+    // return the combined new array
     return namearr.join('.');
 }
 
 // exportable functions
 module.exports = {
-    // this function will make the system calls like before piping the output to append to return.pvl
+    /**
+     * 
+     * @param {string} cubeName 
+     * @param {string} filepath 
+     * @param {string} returnPath 
+     * @param {string} imagePath 
+     * @returns {int} code
+     * @description this function will make the system calls like before piping the output to append to return.pvl
+     */
     makeSystemCalls: function(cubeName, filepath, returnPath, imagePath){
-        
+        // get image name 
         let imagename = getimagename(cubeName, 'png');
-
+        // remove a pvl if it exists
         exec('rm ' + returnPath);
-
         // execute the campt function
-        try{
-            exec('campt from= ' + filepath
+        exec('campt from= ' + filepath
             + " to= " + returnPath + " append= true", function(err, stdout, stderr){
                 if(err){
+                    // print error
                     console.log('Failed campt call');
-                    console.log("Error: " + err);
+                    console.log(err);
                 }
             });
-        }catch(err){
-            console.log('Error running ISIS command campt' + err);
-        }
         
-
         // execute the catlab function
-        try{
-            exec('catlab from= ' + filepath
+        exec('catlab from= ' + filepath
             + " to= " + returnPath + " append= true", function(err, stdout, stderr){
                 if(err){
+                    // print error
                     console.log('Failed catlab call');
-                    console.log("Error: " + err);
+                    console.log(err);
                 }
             });
-        }catch(err){
-            console.log('Error running ISIS command catlab' + err);
-        }
 
         // execute the catoriglab function
-        try{
-            exec('catoriglab from= ' + filepath
+        exec('catoriglab from= ' + filepath
             + " to= " + returnPath + " append= true", function(err, stdout, stderr){
                 if(err){
+                    // log error to console
                     console.log('Failed catoriglab call');
-                    console.log("Error: " + err);
+                    console.log(err);
                 }
             });
-        }catch(err){
-            console.log('Error running ISIS command catoriglab' + err);
-        }
 
         // execute the isis2std function
-        try{
-            exec('isis2std from= ' + filepath
+        exec('isis2std from= ' + filepath
             + " to= " + imagePath + '/' + imagename, function(err, stdout, stderr){
                 if(err){
+                    // log error
                     console.log('Failed isis2std call');
-                    console.log("Error: " + err);
+                    console.log(err);
+                    return 
                 }
             });
-        }catch(err){
-            console.log('Error running ISIS command catoriglab' + err);
-        }
         
-
-        return "func  makeSystemCalls retuned";
+        return 0;
     }
 
-    // Another function
-    // TODO: extractImage
-
-    // Another function
+    // Another expoertable function
 };
